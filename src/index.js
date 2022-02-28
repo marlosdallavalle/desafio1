968224c00d78a64ed180ecaeec59dad4f1281431
marlos.valle <nb25894@novabase.pt>
2022-02-28 09:54:07 +0000

    desafio1 - correcoes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const res = require('express/lib/response');
-const moment = require('moment');
 
 const app = express();
 
@@ -28,7 +27,7 @@ app.post('/users', (request, response) => {
 
   const existsUser = users.some((user) => user.username === username)
   if(existsUser){
-    return response.status(404).json({ error: "User account already exists." })
+    return response.status(400).json({ error: "User account already exists." })
   }
   const user = {
     id: uuidv4(),
@@ -37,13 +36,13 @@ app.post('/users', (request, response) => {
     todos: []
   }
   users.push(user);
-  return response.status(201).send();
+  return response.status(201).send(user);
 
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
-  return response.json(user);
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
@@ -54,11 +53,11 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
     id: uuidv4(),
     title: title,
     done: false,
-    deadline: moment(deadline).format('YYYY-MM-DD'),
-    created_at: moment(new Date()).format('YYYY-MM-DD')
+    deadline: deadline,
+    created_at: new Date()
   }
   user.todos.push(task);
-  return response.status(201).send();
+  return response.status(201).send(task);
 
 });
 
@@ -72,8 +71,8 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
     
     if(task){
       task.title = title;
-      task.deadline = moment(deadline).format('YYYY-MM-DD')
-      return response.status(201).send();
+      task.deadline = deadline
+      return response.status(201).send(task);
     }
   }
 
@@ -90,7 +89,7 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
     
     if(task){
       task.done = true;
-      return response.status(201).send();
+      return response.status(201).send(task);
     }
   }
 
@@ -107,7 +106,7 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
     
     if(task){
       user.todos.splice(task,1);
-      return response.json();
+      return response.status(204).send();
     }
   }
 
